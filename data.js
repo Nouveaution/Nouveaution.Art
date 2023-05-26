138fd802311b4cc09fa2fa669f42aedaf6573a1e
tom-leamon <leamon@outlook.com>
2023-05-25 19:14:10 -0500

    Change order

--- data.js
+++ data.js
@@ -1,89 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-garden",
-      "name": "Garden",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1536,
-      "initialViewParameters": {
-        "yaw": -1.251723645775158,
-        "pitch": 0.15448659349740268,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.1163126304549724,
-          "pitch": 0.06530400523808666,
-          "rotation": 0,
-          "target": "1-rear"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-rear",
-      "name": "Rear",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1536,
-      "initialViewParameters": {
-        "yaw": 0.06979459299541091,
-        "pitch": -0.025257857830961683,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.5643869609903813,
-          "pitch": 0.03615429516415247,
-          "rotation": 0,
-          "target": "0-garden"
-        },
-        {
-          "yaw": 1.4345335813151063,
-          "pitch": 0.19866239359377502,
-          "rotation": 0,
-          "target": "2-entrance-"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-entrance-",
+      "id": "0-entrance-",
       "name": "Entrance ",
       "levels": [
         {
@@ -127,6 +45,88 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
+      "id": "1-rear",
+      "name": "Rear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.06979459299541091,
+        "pitch": -0.025257857830961683,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5643869609903813,
+          "pitch": 0.03615429516415247,
+          "rotation": 0,
+          "target": "2-garden"
+        },
+        {
+          "yaw": 1.4345335813151063,
+          "pitch": 0.19866239359377502,
+          "rotation": 0,
+          "target": "0-entrance-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-garden",
+      "name": "Garden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -1.251723645775158,
+        "pitch": 0.15448659349740268,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1163126304549724,
+          "pitch": 0.06530400523808666,
+          "rotation": 0,
+          "target": "1-rear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
       "id": "3-back",
       "name": "Back",
       "levels": [
@@ -159,7 +159,7 @@ var APP_DATA = {
           "yaw": 1.4468855883613942,
           "pitch": -0.017227653270591503,
           "rotation": 0,
-          "target": "2-entrance-"
+          "target": "0-entrance-"
         }
       ],
       "infoHotspots": [
